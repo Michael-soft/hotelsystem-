52c2abd6eb8b8177db93b0e15d5d369106d20d58
Tosin Michael Akinde <tosin@onefi.local>
2022-06-17 18:19:10 +0100

    edit first

---
+++ Booking_app/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from Customer_app.models import RoomManager,Customer
+
+# Create your models here.
+class Contact(models.Model):
+    occuppant_name = models.CharField(max_length=100)
+    email = models.CharField(max_length=100)
+    occupant_job = models.CharField(max_length=100)
+    message = models.TextField(max_length=1000)
+def _str__(self):
+     return "occuppant_name: "+self.Contact
+  
+
+class Room(models.Model):
+    manager = models.ForeignKey(RoomManager,on_delete=models.CASCADE)
+    room_no = models.CharField(max_length=5, )
+    user_id = models.ForeignKey(Customer, on_delete=models.CASCADE)
+    room_type = models.CharField(max_length=50)
+    is_available = models.BooleanField(default=True)
+    price = models.FloatField(default=1000.00)
+    no_of_night = models.IntegerField()
+    start_date = models.DateField(auto_now=False,auto_now_add=False)
+    end_day = models.DateField(auto_now=False, auto_now_add=False)
+    amount_paid = models.IntegerField() 
+def __str__(self):
+    return "room_no:  "+str(self.user_id)
+
+
---
+++ Booking_app/urls.py
@@ -0,0 +1,12 @@
+from django.urls import path
+
+app_name = "Booking_app"
+
+urlpatterns = [ 
+    path( )
+
+
+    
+
+]
+
